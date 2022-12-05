deca4bb1965cb1e00d7f632fc6ca344f78358105
ivan <shvalov.i@gmail.com>
2022-12-05 11:49:01 +0700

    9

--- exercise_9.py
+++ exercise_9.py
@@ -1,4 +1,18 @@
 def isPalindrome(x: int) -> bool:
-    return str(x) == str(x)[::-1]
+    # Более быстрое решение
+    if x < 0:
+        return False
+    invert_number = 0
+    change_num = x
+    while change_num > 0:
+        invert_number = invert_number * 10 + change_num % 10
+        change_num = change_num // 10
+    return invert_number == x
+    # Простое решение
+    # return str(x) == str(x)[::-1]
+
+print(isPalindrome(101))
+print(isPalindrome(1011))
+print(isPalindrome(-121))
 
 
